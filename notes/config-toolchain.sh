39980a9eb5b9abdf10d7f8bc3dae658249292a5d
jameshloving <jameshloving@gmail.com>
2017-01-26 16:24:48 -0500

    Update toolchain config script & documentation

--- notes/config-toolchain.sh
+++ notes/config-toolchain.sh
@@ -1,11 +1,26 @@
-STAGING_DIR='/home/ubuntu/lede-sdk-brcm47xx-mips74k_gcc-5.4.0_musl-1.1.15.Linux-x86_64/staging_dir'
-TOOLCHAIN_DIR='toolchain-mipsel_74kc_gcc-5.4.0_musl-1.1.15'
+#!/bin/bash
 
-export AR=$STAGING_DIR/$TOOLCHAIN_DIR/bin/your-architecture-ar
-export CC=$STAGING_DIR/$TOOLCHAIN_DIR/bin/your-architecture-gcc
-export CXX=$STAGING_DIR/$TOOLCHAIN_DIR/bin/your-architecture-g++
-export LINK=$STAGING_DIR/$TOOLCHAIN_DIR/bin/your-architecture-g++
-export RANLIB=$STAGING_DIR/$TOOLCHAIN_DIR/bin/your-architecture-ranlib
+################################################################################
+#
+#   Name:        config-toolchain.sh
+#   Authors:     James H. Loving
+#   Description: This script is used to configure the environment variables
+#                to allow for the LEDE SDK toolchain to cross-compile.
+#
+#                This script is currently configured for:
+#                Linksys WRT1200AC
+#
+################################################################################
+
+STAGING_DIR='/home/ubuntu/github/lede/staging_dir'
+TOOLCHAIN_DIR='toolchain-arm_cortex-a9+vfpv3_gcc-5.4.0_musl-1.1.16_eabi'
+ARCH='arm-openwrt-linux'
+
+export AR=$STAGING_DIR/$TOOLCHAIN_DIR/bin/$ARCH-ar
+export CC=$STAGING_DIR/$TOOLCHAIN_DIR/bin/$ARCH-gcc
+export CXX=$STAGING_DIR/$TOOLCHAIN_DIR/bin/$ARCH-g++
+export LINK=$STAGING_DIR/$TOOLCHAIN_DIR/bin/$ARCH-g++
+export RANLIB=$STAGING_DIR/$TOOLCHAIN_DIR/bin/$ARCH-ranlib
 export STAGING_DIR=$STAGING_DIR
 export LIBPATH=$STAGING_DIR/$TOOLCHAIN_DIR/lib
 export LDFLAGS='-Wl,-rpath-link '${LIBPATH}
